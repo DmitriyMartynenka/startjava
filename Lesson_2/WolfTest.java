0536c0ed619b44e7cb762d5a32531ad84b62722a
DmitriyMartynenka <dmitriymartyn9797@mail.ru>
2021-04-07 23:37:03 +0300

    corrected task 2

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,32 +1,3 @@
-class Wolf {
-	
-	String sex;
-	String nickname;
-	int weight;
-	int age;
-	String color;
-
-	void go() {
-		System.out.println("Идет");
-	}
-
-	void sit() {
-		System.out.println("Сидит");
-	}
-
-	void run() {
-		System.out.println("Бежит");
-	}
-
-	void howl() {
-		System.out.println("Воет");
-	}
-
-	void hunt() {
-		System.out.println("Охотиться");
-	}
-}
-
 public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolf1 = new Wolf();
@@ -36,7 +7,8 @@ public class WolfTest {
 		wolf1.age = 3;
 		wolf1.color = "grey";
 
-		System.out.println("Wolf information: sex - " + wolf1.sex + ", nickname - " + wolf1.nickname + ", weight - " + wolf1.weight + ", age - " + wolf1.age + ", color - " + wolf1.color);
+		System.out.println("Wolf information: sex - " + wolf1.sex + ", nickname - " + wolf1.nickname + 
+			", weight - " + wolf1.weight + ", age - " + wolf1.age + ", color - " + wolf1.color);
 
 		wolf1.go();
 		wolf1.sit();
