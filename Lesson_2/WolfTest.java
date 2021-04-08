e417e6bf88aa6f8926a99dd6d66863177e783830
DmitriyMartynenka <dmitriymartyn9797@mail.ru>
2021-04-08 21:45:41 +0300

    modified Wolf and WolfTest (task 3)

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,14 +1,14 @@
 public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolf1 = new Wolf();
-		wolf1.sex = "female";
-		wolf1.nickname = "Beast";
-		wolf1.weight = 60;
-		wolf1.age = 3;
-		wolf1.color = "grey";
+		wolf1.setSex("male");
+		wolf1.setNickname("Beast");
+		wolf1.setWeight(60);
+		wolf1.setAge(6);
+		wolf1.setColor("grey");
 
-		System.out.println("Wolf information: sex - " + wolf1.sex + ", nickname - " + wolf1.nickname + 
-			", weight - " + wolf1.weight + ", age - " + wolf1.age + ", color - " + wolf1.color);
+		System.out.println("Wolf information: sex - " + wolf1.getSex() + ", nickname - " + wolf1.getNickname() + 
+			", weight - " + wolf1.getWeight() + ", age - " + wolf1.getAge() + ", color - " + wolf1.getColor());
 
 		wolf1.go();
 		wolf1.sit();
